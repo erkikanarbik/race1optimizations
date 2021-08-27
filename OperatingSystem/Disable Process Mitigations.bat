6a40c85b317183255fc9dff3fd29d57895be9a0f
Erki Kanarbik <erkikanarbik@gmail.com>
2021-08-27 09:38:30 +0300

    1

--- OperatingSystem/Disable Process Mitigations.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-@echo Script made by Danske, tweaks found by Melody.
-@echo.
-@echo Disable Process Mitigations
-powershell "ForEach($v in (Get-Command -Name \"Set-ProcessMitigation\").Parameters[\"Disable\"].Attributes.ValidValues){Set-ProcessMitigation -System -Disable $v.ToString() -ErrorAction SilentlyContinue}"
-powershell "Remove-Item -Path \"HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\*\" -Recurse -ErrorAction SilentlyContinue"
-@echo.
-pause
